74229ad3d315f0c4f5bb3d6a08611670566544e9
Le Minh Ngoc <78473023+le-mn@users.noreply.github.com>
2023-10-26 19:24:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+window.addEventListener("scroll", function() {
+    var pageFooter = document.getElementById("page-footer");
+
+    // Check if the user has scrolled to the end of the content
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        // Display the footer when the end is reached
+        pageFooter.style.display = "block";
+    } else {
+        // Hide the footer if not at the end
+        pageFooter.style.display = "none";
+    }
+});
+
+// Your existing JavaScript code can go here or continue below this section.
